ee34b8b6b15e7df2424eb3644e1c01939fa107e8
titasinokaitis <titas.inokaitis@gmail.com>
2025-07-09 11:50:19 +0300

    up

--- src/index.js
+++ src/index.js
@@ -1,18 +1,26 @@
 import express from 'express';
 import { PORT } from './env.js';
-import { PageError404 } from './pages/Error404.js';
-import { publicRouter } from './routes/publicRouter.js';
+import { PageError404 } from './pages/public/Error404.js';
+import { publicPageRouter } from './routes/publicPageRouter.js';
 import { publicApiRouter } from './routes/publicApiRouter.js';
+import { adminPageRouter } from './routes/adminPageRouter.js';
+import { cookieParser } from './middleware/cookieParser.js';
+import { userData } from './middleware/userData.js';
+import { isAdmin } from './middleware/isAdmin.js';
 
 const app = express();
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(cookieParser);
+app.use(userData);
 
-app.use('/', publicRouter);
+app.use('/', publicPageRouter);
 app.use('/', publicApiRouter);
+app.use('/admin', isAdmin, adminPageRouter);
 
 app.get('*error', (req, res) => {
-    return res.send(new PageError404().render());
+    return res.send(new PageError404(req).render());
 });
 
 app.listen(PORT, () => {
